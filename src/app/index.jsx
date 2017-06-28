ac0f49d07719d74f0c259730f5ebd80c6a4265fa
BachPham-PC\BachPham <p.chi.bach@gmail.com>
2017-06-28 22:41:26 +0700

    test

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import {render} from 'react-dom';
+import Input from './style.jsx';
 class Main extends React.Component{
 	render(){
-		return <h1> Say hi </h1>;
+		
+		return (
+
+			<div> 
+				<form >
+					<input type="text" name="query"></input>
+				</form>
+			</div>
+		);
 	}
 }
 render(<Main/> , document.getElementById('app'));
---
+++ src/app/style.jsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+	width: 50%;
+	margin: 0 auto;
+`
+const H1 = styled.h1`
+	text-align: center;
+`
+const P = styled.p`
+	text-align: center;
+`
+const RightBubble = styled.div`
+	max-width: 60%;
+	margin-right: 0;
+	margin-left: auto;
+	margin-bottom: 10px;
+	text-align: right;
+	background : cyan;
+	display: table;
+	padding: 10px;
+`
+
+const LeftBubble = styled.div`
+	max-width: 60%;
+	margin-right: auto;
+	margin-left: 0;
+	margin-bottom: 10px;
+	text-align: left;
+	background : salmon;
+	display: table;
+	padding: 10px;
+`
+const Input = styled.input`
+	#!/usr/bin/env padding:5px; 
+    border:2px solid #ccc; 
+    -webkit-border-radius: 5px;
+    border-radius: 5px;
+`
+export {Div,H1,P,RightBubble,LeftBubble,Input}
